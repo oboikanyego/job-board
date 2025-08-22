9e2868fb6565a421c19d150ed6139c0b9a786698
oboikanyego <kanyiradipabe@gmail.com>
2025-08-22 16:25:16 +0200

    Auth service updated(login,register api exposed), and enviroment file added

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,8 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Auth {
-  
+  private base = `${environment.apiUrl}/auth`;
+
+  constructor(private http: HttpClient) {}
+
+  login(data: any) {
+    return this.http.post<any>(`${this.base}/login`, data);
+  }
+  register(data: any) {
+    return this.http.post<any>(`${this.base}/register`, data);
+  }
+
+  saveAuth(token: string, user: any) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  get user() {
+    return JSON.parse(localStorage.getItem('user') || 'null');
+  }
+  role() {
+    return this.user?.role;
+  }
 }
