9f8c7d4379bacb59efc0900ed973ad760cf951a0
oboikanyego <kanyiradipabe@gmail.com>
2025-08-22 16:27:42 +0200

    get,list and create job Endpoint intro

--- src/app/services/jobs.ts
+++ src/app/services/jobs.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Jobs {
-  
+  private base = `${environment.apiUrl}/jobs`;
+  constructor(private http: HttpClient) {}
+
+  create(job: any) {
+    return this.http.post<any>(this.base, job);
+  }
+  list() {
+    return this.http.get<any[]>(this.base);
+  }
+  get(id: string) {
+    return this.http.get<any>(`${this.base}/${id}`);
+  }
 }
