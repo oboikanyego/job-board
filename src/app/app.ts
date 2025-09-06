dc23f48d9bafa96ec1fd4ad5fba0804129f1e061
oboikanyego <kanyiradipabe@gmail.com>
2025-09-06 18:56:31 +0200

    code clean up,navigation set-up per role

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,6 +9,12 @@ import { CommonModule } from '@angular/common';
 import { Auth } from './services/auth';
 import { Common } from './services/common';
 import { LoaderComponent } from './loader/loader-component/loader-component';
+
+interface NavItem {
+  label: string;
+  path: string;
+  roles: string[]; // which roles can see this
+}
 @Component({
   selector: 'app-root',
   imports: [
@@ -26,7 +32,17 @@ import { LoaderComponent } from './loader/loader-component/loader-component';
 export class App {
   protected readonly title = signal('job-board');
   isUserAuthrized: boolean = false;
-  constructor(public commonService: Common) {
+  userRole ="";
+
+  navItems: NavItem[] =[]
+  constructor(public commonService: Common,private authService: Auth) {
     this.isUserAuthrized = this.commonService.IsUserSignedIn;
+    this.userRole =this.authService.role();
+    this.navItems =  [
+    { label: 'Applications', path: '/applications', roles: ['candidate'] },
+    { label: 'Jobs', path: '/jobs', roles: ['employer'] },
+    { label: 'Profile', path: '/results', roles: ['candidate', 'employer'] },
+    { label: 'Subscription', path: '/reviews', roles: ['employer'] }
+  ];
   }
 }
