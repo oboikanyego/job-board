d6a14ba8d5303e5d6465379d729a5c21980fa8c4
oboikanyego <kanyiradipabe@gmail.com>
2025-09-06 17:21:22 +0200

    formated the code

--- src/app/services/loader-service.ts
+++ src/app/services/loader-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoaderService {
   private loading = new BehaviorSubject<boolean>(false);
